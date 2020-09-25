0ddfcdbbcfa85ba3941a31b959aadc31a3a874ad
IrfanAdnan <irfan.adnan150@gmail.com>
2020-09-25 16:28:09 +0800

    upload jQuery

---
+++ ProjekjQuery/script.js
@@ -0,0 +1,35 @@
+var num1;
+var num2;
+var guess;
+var answer;
+var response;
+
+$(document).ready(function() {
+    num1 = document.getElementById("num1");
+    num2 = document.getElementById("num2");
+    guess = document.getElementById("inputfield");
+    response = document.getElementById("response")
+
+    $("#submitbutton").click(function() {
+        checkAnswer();
+    });
+
+    setNumbers();
+});
+
+function setNumbers() {
+    num1.innerHTML = Math.floor(Math.random() * 10) + 1;
+    num2.innerHTML = Math.floor(Math.random() * 10) + 1;
+}
+
+function checkAnswer() {
+    var n1 = parseInt(num1.innerHTML);
+    var n2 = parseInt(num2.innerHTML);
+    answer = n1 * n2;
+
+    if(parseInt(guess.value) == answer){
+         response.innerHTML = "Correct";
+         setNumbers();
+    }else 
+         response.innerHTML = "Incorrect";
+}
